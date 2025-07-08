7326816eccfcb6b488f43eafe9b0585fc8f8fcf1
LPATROCINIO69 <luciopp69@gmail.com>
2025-07-08 13:26:41 -0300

    Criação das pastas controllers, routes, services e utils

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+import express, {json, Request, Response} from "express";
+import createApp from "./app";
+
+const app = createApp();
+const port = process.env.PORT;
+
+
+app.listen(port,()=>{
+    console.log(`🚨 Server ativo na porta http://localhost:${port}`);
+});
+
+
