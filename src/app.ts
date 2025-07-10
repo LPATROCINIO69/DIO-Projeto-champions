dee152870846d2f6a1667241a4c6d28600d1a1d5
LPATROCINIO69 <luciopp69@gmail.com>
2025-07-10 09:55:59 -0300

    inclusão das camadas referentes aos clubs

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, {json, Request,Response} from "express";
 import { getPlayer } from "./controllers/players-controller";
 import router from "./routes/routes";
+import cors from 'cors';
 
 
 function createApp(){
     const app = express();
     app.use(json());
     app.use('/api', router);
+    
+    const corsOptions = {
+        origin:"http://localhost",
+        methods:["GET","POST","DELETE","PATCH"]
+    }
+    app.use(cors(corsOptions)); 
     return app;
 }
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
 import {Router} from "express";
 import { deletePlayer, getPlayer, getPlayerById, postPlayer, updatePlayer } from "../controllers/players-controller";
+import { getClubs } from "../controllers/clubs-controller";
 
 const router = Router();
 
@@ -8,5 +9,8 @@ router.get("/players/:id",getPlayerById);       // busca um jogador pelo id
 router.post("/players", postPlayer);            // inclui um novo jogador ao BD
 router.delete("/players/:id",deletePlayer);     // Exclui um jogador pelo id
 router.patch("/players/:id",updatePlayer);      // Atualiza as informações de um jogador
+
+
+router.get("/clubs", getClubs);                         // busca a lista de clubs
 
 export default router;
